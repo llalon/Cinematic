f10889fc695bd00ec99767bccd6daba5012f1dc4
llalon <71791017+llalon@users.noreply.github.com>
2026-02-09 23:33:14 -0500

    Update ClientContext.java

--- src/main/java/de/llalon/cinematic/domain/ClientContext.java
+++ src/main/java/de/llalon/cinematic/domain/ClientContext.java
@@ -14,6 +14,7 @@ import de.llalon.cinematic.client.sonarr.SonarrClient;
 import de.llalon.cinematic.client.sonarr.config.SonarrProperties;
 import de.llalon.cinematic.client.tautulli.TautulliClient;
 import de.llalon.cinematic.client.tautulli.config.TautulliProperties;
+import java.util.function.Supplier;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -37,6 +38,7 @@ public final class ClientContext {
             QBittorrentClient qbittorrentClient,
             OverseerrClient overseerrClient,
             TautulliClient tautulliClient) {
+        this.httpClient = httpClient == null ? new OkHttpClient() : httpClient;
         this.objectMapper = objectMapper == null
                 ? JsonMapper.builder()
                         .addModule(new JavaTimeModule())
@@ -44,32 +46,80 @@ public final class ClientContext {
                         .build()
                 : objectMapper;
 
-        this.httpClient = httpClient == null ? new OkHttpClient() : httpClient;
+        // Radarr
+        if (radarrClient != null) {
+            this.radarrClient = radarrClient;
+            this.radarrProperties = radarrProperties;
+        } else {
+            this.radarrProperties = radarrProperties != null
+                    ? radarrProperties
+                    : safeFromEnv(RadarrProperties::fromEnvironment, "Radarr");
+            this.radarrClient = this.radarrProperties != null
+                    ? new RadarrClient(this.httpClient, this.radarrProperties, this.objectMapper)
+                    : null;
+        }
 
-        this.radarrProperties = radarrProperties == null ? RadarrProperties.fromEnvironment() : radarrProperties;
-        this.sonarrProperties = sonarrProperties == null ? SonarrProperties.fromEnvironment() : sonarrProperties;
-        this.qbittorrentProperties =
-                qbittorrentProperties == null ? QBittorrentProperties.fromEnvironment() : qbittorrentProperties;
-        this.overseerrProperties =
-                overseerrProperties == null ? OverseerrProperties.fromEnvironment() : overseerrProperties;
-        this.tautulliProperties =
-                tautulliProperties == null ? TautulliProperties.fromEnvironment() : tautulliProperties;
+        // Sonarr
+        if (sonarrClient != null) {
+            this.sonarrClient = sonarrClient;
+            this.sonarrProperties = sonarrProperties;
+        } else {
+            this.sonarrProperties = sonarrProperties != null
+                    ? sonarrProperties
+                    : safeFromEnv(SonarrProperties::fromEnvironment, "Sonarr");
+            this.sonarrClient = this.sonarrProperties != null
+                    ? new SonarrClient(this.httpClient, this.sonarrProperties, this.objectMapper)
+                    : null;
+        }
 
-        this.radarrClient = radarrClient == null
-                ? new RadarrClient(this.httpClient, this.radarrProperties, this.objectMapper)
-                : radarrClient;
-        this.sonarrClient = sonarrClient == null
-                ? new SonarrClient(this.httpClient, this.sonarrProperties, this.objectMapper)
-                : sonarrClient;
-        this.qbittorrentClient = qbittorrentClient == null
-                ? new QBittorrentClient(this.httpClient, this.qbittorrentProperties, this.objectMapper)
-                : qbittorrentClient;
-        this.overseerrClient = overseerrClient == null
-                ? new OverseerrClient(this.httpClient, this.overseerrProperties, this.objectMapper)
-                : overseerrClient;
-        this.tautulliClient = tautulliClient == null
-                ? new TautulliClient(this.httpClient, this.tautulliProperties, this.objectMapper)
-                : tautulliClient;
+        // QBittorrent
+        if (qbittorrentClient != null) {
+            this.qbittorrentClient = qbittorrentClient;
+            this.qbittorrentProperties = qbittorrentProperties;
+        } else {
+            this.qbittorrentProperties = qbittorrentProperties != null
+                    ? qbittorrentProperties
+                    : safeFromEnv(QBittorrentProperties::fromEnvironment, "QBittorrent");
+            this.qbittorrentClient = this.qbittorrentProperties != null
+                    ? new QBittorrentClient(this.httpClient, this.qbittorrentProperties, this.objectMapper)
+                    : null;
+        }
+
+        // Overseerr
+        if (overseerrClient != null) {
+            this.overseerrClient = overseerrClient;
+            this.overseerrProperties = overseerrProperties;
+        } else {
+            this.overseerrProperties = overseerrProperties != null
+                    ? overseerrProperties
+                    : safeFromEnv(OverseerrProperties::fromEnvironment, "Overseerr");
+            this.overseerrClient = this.overseerrProperties != null
+                    ? new OverseerrClient(this.httpClient, this.overseerrProperties, this.objectMapper)
+                    : null;
+        }
+
+        // Tautulli
+        if (tautulliClient != null) {
+            this.tautulliClient = tautulliClient;
+            this.tautulliProperties = tautulliProperties;
+        } else {
+            this.tautulliProperties = tautulliProperties != null
+                    ? tautulliProperties
+                    : safeFromEnv(TautulliProperties::fromEnvironment, "Tautulli");
+            this.tautulliClient = this.tautulliProperties != null
+                    ? new TautulliClient(this.httpClient, this.tautulliProperties, this.objectMapper)
+                    : null;
+        }
+    }
+
+    private static <T> T safeFromEnv(Supplier<T> supplier, String name) {
+        try {
+            log.debug("Creating {} properties from environment variables", name);
+            return supplier.get();
+        } catch (IllegalArgumentException | NullPointerException exception) {
+            log.warn("{} properties could not be initialized from environment variables", name, exception);
+            return null;
+        }
     }
 
     private static ClientContext INSTANCE;
