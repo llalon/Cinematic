97cb1a506125658089fca4b7e72b1afed3696e93
llalon <71791017+llalon@users.noreply.github.com>
2026-02-13 00:46:41 -0500

    save

--- src/main/java/de/llalon/cinematic/domain/Movie.java
+++ src/main/java/de/llalon/cinematic/domain/Movie.java
@@ -1,3 +1,26 @@
 package de.llalon.cinematic.domain;
 
-public class Movie {}
+import de.llalon.cinematic.client.radarr.dto.MovieResource;
+import de.llalon.cinematic.client.radarr.dto.TagResource;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Movie {
+
+    private final ClientContext context;
+    private final MovieResource radarrMovie;
+
+    public Movie(ClientContext context, MovieResource radarrMovie) {
+        this.context = context;
+        this.radarrMovie = radarrMovie;
+    }
+
+    public Iterable<Tag> tags() {
+        final Map<Integer, String> tags = context.getRadarrClient().getAllTags().stream()
+                .collect(Collectors.toMap(TagResource::getId, TagResource::getLabel));
+
+        return radarrMovie.getTags().stream()
+                .map(tagId -> new Tag(context, tags.get(tagId)))
+                .toList();
+    }
+}
--- src/main/java/de/llalon/cinematic/domain/Tag.java
+++ src/main/java/de/llalon/cinematic/domain/Tag.java
@@ -1,3 +1,30 @@
 package de.llalon.cinematic.domain;
 
-public class Tag {}
+public class Tag {
+
+    private final ClientContext context;
+    private final String name;
+
+    public Tag(ClientContext context, String name) {
+        this.context = context;
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Iterable<Movie> movies() {
+        return context.getRadarrClient().getAllMovies().stream()
+                .map(x -> new Movie(context, x))
+                .toList();
+    }
+
+    public Iterable<Series> series() {
+        return null;
+    }
+
+    public Iterable<Torrent> torrents() {
+        return null;
+    }
+}
